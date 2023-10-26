2222557671707f4515e52072295d4369f9fc0f68
202111 <202111@1305-007>
2023-10-26 11:10:10 +0300

    Почти сделал добавление фоторграфий питомцов

--- Animal_Info/Pages/AddPhotoMenu.xaml.cs
+++ Animal_Info/Pages/AddPhotoMenu.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Animal_Info.BD;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Configuration;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +27,23 @@ namespace Animal_Info.Pages
         public AddPhotoMenu()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Animal animals = new Animal();
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Filter = "*.png|*.png|*.jpeg|*.jpeg|*.jpg|*.Jpg"
+            };
+            if (openFileDialog.ShowDialog().GetValueOrDefault())
+            {
+                animals.Photo = File.ReadAllBytes(openFileDialog.FileName);
+                .Source = new BitmapImage(new Uri(openFileDialog.FileName));
+
+                App.BD.Animal.Add(animals);
+                App.BD.SaveChanges();
+            }
         }
     }
 }
